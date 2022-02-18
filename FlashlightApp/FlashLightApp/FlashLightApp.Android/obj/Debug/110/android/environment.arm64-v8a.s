f4f7435569e5b031bfa1e69edac21d338dc640dd
Jacaguas <54630015+Jacaguas@users.noreply.github.com>
2022-02-18 02:42:30 -0500

    final changes with const var

--- FlashlightApp/FlashLightApp/FlashLightApp.Android/obj/Debug/110/android/environment.arm64-v8a.s
+++ FlashlightApp/FlashLightApp/FlashLightApp.Android/obj/Debug/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"820365dd-3e7c-4fe2-86fe-12ec2f4b7c04"
+	.asciz	"79d40252-c996-4ad8-a727-c48e70a11653"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- FlashlightApp/FlashLightApp/FlashLightApp/Utility.cs
+++ FlashlightApp/FlashLightApp/FlashLightApp/Utility.cs
@@ -8,17 +8,19 @@ namespace FlashLightApp
 {
     public static class Utility
     {
+        const int DELAY_BEFORE_LETTERS = 150;
+
         public static async Task ShortFlash()
         {          
-                Task.Delay(100).Wait();
+                Task.Delay(DELAY_BEFORE_LETTERS).Wait();
                 await Flashlight.TurnOnAsync();
-                Task.Delay(300).Wait();
+                Task.Delay(250).Wait();
                 await Flashlight.TurnOffAsync();            
         }
 
         public static async Task LongFlash()
         {
-                Task.Delay(100).Wait();
+                Task.Delay(DELAY_BEFORE_LETTERS).Wait();
                 await Flashlight.TurnOnAsync();
                 Task.Delay(700).Wait();
                 await Flashlight.TurnOffAsync();           
@@ -26,12 +28,14 @@ namespace FlashLightApp
 
         public static async Task A()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();            
             await LongFlash();
            
         }
         public static async Task B()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
 
             await LongFlash();
             await ShortFlash();
@@ -41,6 +45,7 @@ namespace FlashLightApp
         }
         public static async Task C()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
 
             await LongFlash();
             await ShortFlash();
@@ -50,6 +55,7 @@ namespace FlashLightApp
         }
         public static async Task D()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
 
             await LongFlash();
             await ShortFlash();
@@ -57,11 +63,13 @@ namespace FlashLightApp
 
         }
         public static async Task E()
-        {            
+        {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();           
         }
         public static async Task F()
-        {            
+        {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();            
             await ShortFlash();
             await LongFlash();
@@ -69,12 +77,14 @@ namespace FlashLightApp
         }
         public static async Task G()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await LongFlash();
             await ShortFlash();
         }
         public static async Task H()
-        {            
+        {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await ShortFlash();
             await ShortFlash();
@@ -82,25 +92,29 @@ namespace FlashLightApp
         }
         public static async Task I()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await ShortFlash();
         }
         public static async Task J()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await LongFlash();
             await LongFlash();
             await LongFlash();
         }
         public static async Task K()
-        {            
+        {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await ShortFlash();
             await LongFlash();
             
         }
         public static async Task L()
-        {            
+        {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await LongFlash();
             await ShortFlash();
@@ -108,24 +122,28 @@ namespace FlashLightApp
         }
         public static async Task M()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await LongFlash();
 
         }
         public static async Task N()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await ShortFlash();
            
         }
         public static async Task O()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await LongFlash();
             await LongFlash();
         }
         public static async Task P()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await LongFlash();            
             await LongFlash();
@@ -133,35 +151,41 @@ namespace FlashLightApp
         }
         public static async Task Q()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await LongFlash();
             await ShortFlash();
             await LongFlash();
         }
         public static async Task R()
-        {            
+        {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await LongFlash();
             await ShortFlash();
         }
         public static async Task S()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await ShortFlash();
             await ShortFlash();
         }
         public static async Task T()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
         }
         public static async Task U()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();            
             await ShortFlash();
             await LongFlash();
         }
         public static async Task V()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();
             await ShortFlash();
             await ShortFlash();
@@ -169,12 +193,14 @@ namespace FlashLightApp
         }
         public static async Task W()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await ShortFlash();            
             await LongFlash();
             await LongFlash();
         }
         public static async Task X()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await ShortFlash();
             await ShortFlash();
@@ -182,6 +208,7 @@ namespace FlashLightApp
         }
         public static async Task Y()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();
             await ShortFlash();
             await LongFlash();
@@ -189,6 +216,7 @@ namespace FlashLightApp
         }
         public static async Task Z()
         {
+            Task.Delay(DELAY_BEFORE_LETTERS).Wait();
             await LongFlash();            
             await LongFlash();
             await ShortFlash();
